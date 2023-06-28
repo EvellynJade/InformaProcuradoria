25715d59d80b7defa127d03f60a7e58f0fa25a16
Evellyn Jade <evellyn.reis@dcomp.ufs.br>
2023-06-28 11:04:01 -0300

    Implementação do login e homepage

---
+++ src/pages/HomePage/index.js
@@ -0,0 +1,31 @@
+import React  from "react";
+import NavBar from "../../components/navbar";
+import Footer from "../../components/footer";
+
+const HomePage = () => {
+    return (
+        <div className="home">
+            <NavBar />
+            <br/>
+            <br/>
+            <br/>
+            <br/>
+            <br/>
+            <br/>
+            <br/>
+            <br/>
+            <br/>
+            <br/>
+            <br/>
+            <br/>
+            <br/>
+            <br/>
+            <br/>
+            <br/>
+            <br/>
+            <Footer />
+        </div>    
+    );
+}
+
+export default HomePage;
